da2e89e4087a0020eeb3e2e100cac2a7e9c0a305
Mohamed Hassan <mohamed.h.ismael@gmail.com>
2025-08-25 18:11:36 +0300

    feat(user): add virtual message

--- src/config/models/user.model.js
+++ src/config/models/user.model.js
@@ -58,28 +58,38 @@ const schema = new Schema(
 
   {
     timestamps: true,
-    toJSON: {
-      getters: true,
-      transform: function (doc, ret) {
-        ret.id = ret._id;
-        delete ret._id;
-        delete ret.__v;
-        delete ret.password;
-        return ret;
-      },
-    },
-    toObject: {
-      getters: true,
-      transform: function (doc, ret) {
-        ret.id = ret._id;
-        delete ret._id;
-        delete ret.__v;
-        delete ret.password;
-        return ret;
-      },
-    },
+    toJSON: { getters: true, virtuals: true, transform: commonTransform },
+    toObject: { getters: true, virtuals: true, transform: commonTransform },
   }
 );
+
+schema.virtual("message").get(function () {
+  const hours = new Date().getHours();
+
+  const timeMsg =
+    hours < 12
+      ? "Good morning"
+      : hours < 18
+      ? "Good afternoon"
+      : "Good evening";
+
+  const title = this.gender === Gender.male ? "Mr." : "Ms.";
+
+  return `${timeMsg}, ${title} ${this.name}`;
+});
+
+function commonTransform(doc, ret) {
+  const message = ret.greeting;
+  delete ret.greeting;
+  return {
+    message,
+    id: ret._id,
+    ...ret,
+    _id: undefined,
+    __v: undefined,
+    password: undefined,
+  };
+}
 
 const userModel = model("users", schema);
 export default userModel;
